6e5d5cacfb6f2d4fc847b18d713c3e38b99f3150
chana perel kats <chpkats@gmail.com>
2023-10-25 20:15:58 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program5494.cs
@@ -0,0 +1,19 @@
+﻿namespace Stage0;
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome5494();
+        welcome8536();
+        Console.ReadKey();
+    }
+
+    static partial void welcome8536();
+    private static void welcome5494()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine(name + ", welcome to my first console application");
+    }
+
+}
