9468b4ede5fa7996388e2cdc1d75c79dfa8e735d
tsw1212 <tsw32595@gmail.com>
2023-11-06 16:22:16 +0200

    finish Task

--- DalFacade/DO/Task.cs
+++ DalFacade/DO/Task.cs
@@ -2,29 +2,29 @@
 
 namespace DO;
 
- public record Task
+public record Task
 {
-    int Id;
-    string Description;
-    string Ailas;
-    bool IsMilestone;
-    DateTime CreatedAtDete;
-    DateTime StartDete;
-    DateTime ScheduledDete;
-    DateTime ForecasDate;
-    DateTime DeadLineDate;
-    DateTime CompleteDate;
-    string Deliverables;
-    string Remarks;
-    int EngineerId;
+    int? Id { get; set; } = null;
+    string Description { get; set; }
+    string Ailas { get; set; }
+    bool IsMilestone { get; set; }
+    DateTime CreatedAtDete { get; set; }
+    DateTime StartDete { get; set; }
+    DateTime? ScheduledDete { get; set; } = null;
+    DateTime? ForecasDate { get; set; } =  null;
+    DateTime DeadLineDate { get; set; }
+    DateTime? CompleteDate { get; set; } = null;
+    string Deliverables { get; set; }
+    string Remarks { get; set; }
+    int EngineerId { get; set; }
     /*לא גמור*/
-    int ComplexityLevl;
+    int ComplexityLevl { get; set; }
 
     public Task()
     {
 
     }
-    public Task(int Id,string Description,string Ailas,bool IsMilestone ,DateTime CreatedAtDete, DateTime StartDete ,DateTime ScheduledDete,DateTime ForecasDate,
+    public Task(string Description,string Ailas,bool IsMilestone ,DateTime CreatedAtDete, DateTime StartDete ,DateTime ScheduledDete,DateTime ForecasDate,
     DateTime DeadLineDate,DateTime CompleteDate,string Deliverables,string Remarks,int EngineerId, /*לא גמור*/int ComplexityLevl)
     {
         this.Id = Id;
@@ -40,5 +40,6 @@ namespace DO;
         this.Remarks = Remarks;
         this.EngineerId = EngineerId;
         this.CompleteDate = CompleteDate;
+
     }
 }
