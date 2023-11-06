0a3927f183f8a55c1757f044aa52060c48e86404
tsw1212 <tsw32595@gmail.com>
2023-11-06 02:13:17 +0200

    Task

---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace DO;
+
+ public record Task
+{
+    int Id;
+    string Description;
+    string Ailas;
+    bool IsMilestone;
+    DateTime CreatedAtDete;
+    DateTime StartDete;
+    DateTime ScheduledDete;
+    DateTime ForecasDate;
+    DateTime DeadLineDate;
+    DateTime CompleteDate;
+    string Deliverables;
+    string Remarks;
+    int EngineerId;
+    /*לא גמור*/
+    int ComplexityLevl;
+
+    public Task()
+    {
+
+    }
+    public Task(int Id,string Description,string Ailas,bool IsMilestone ,DateTime CreatedAtDete, DateTime StartDete ,DateTime ScheduledDete,DateTime ForecasDate,
+    DateTime DeadLineDate,DateTime CompleteDate,string Deliverables,string Remarks,int EngineerId, /*לא גמור*/int ComplexityLevl)
+    {
+        this.Id = Id;
+        this.Description = Description;
+        this.Ailas = Ailas;     
+        this.IsMilestone = IsMilestone;
+        this.CreatedAtDete= CreatedAtDete;
+        this.StartDete = StartDete;
+        this.ScheduledDete= ScheduledDete;
+        this.ForecasDate = ForecasDate;
+        this.DeadLineDate = DeadLineDate;
+        this.Deliverables = Deliverables; 
+        this.Remarks = Remarks;
+        this.EngineerId = EngineerId;
+        this.CompleteDate = CompleteDate;
+    }
+}
