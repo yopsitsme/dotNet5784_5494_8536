9ea1a728c19981c421a30cdf466ed9d8c867c5ff
tsw1212 <tsw32595@gmail.com>
2023-11-06 16:53:21 +0200

    +dependency

---
+++ DalFacade/DO/dependency.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace DO;
+
+public record dependency
+{
+    int Id { get; set; }
+    int DependentTask {  get; set; }
+    int DependsTask {  get; set; }
+
+    public dependency() { }
+    public dependency(int Id, int DependentTask, int DependsTask) 
+    { 
+        this.Id = Id;   
+        this.DependentTask = DependentTask;     
+        this.DependentTask = DependsTask;
+    }
+
+
+
+}
