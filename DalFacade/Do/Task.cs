70f9ed170040c9af3250704d3ee42b60464b6c45
chana perel kats <chpkats@gmail.com>
2023-11-06 23:14:09 +0200

    try

--- DalFacade/Do/Task.cs
+++ DalFacade/Do/Task.cs
@@ -4,22 +4,6 @@ namespace DO;
 
 public record Task
 {
-<<<<<<< HEAD
-    public int Id { get; set; }
-    string Description { get; set; }
-    string Ailas { get; set; }
-    bool IsMilestone { get; set; }
-    DateTime CreatedAtDete { get; set; }
-    DateTime StartDete { get; set; }
-    DateTime? ScheduledDete { get; set; } = null;
-    DateTime? ForecasDate { get; set; } = null;
-    DateTime DeadLineDate { get; set; }
-    DateTime? CompleteDate { get; set; } = null;
-    string Deliverables { get; set; }
-    string Remarks { get; set; }
-    EngineerExperience‏ EngineerId { get; set; }
-    int ComplexityLevl { get; set; }
-=======
  public  int Id { get; set; } 
     public string Description { get; set; }
     public string Ailas { get; set; }
@@ -33,16 +17,13 @@ public record Task
     public string Deliverables { get; set; }
     public string Remarks { get; set; }
     public int EngineerId { get; set; }
-    /*לא גמור*/
     public EngineerExperience‏ ComplexityLevl { get; set; }
->>>>>>> e4bca0acc3730ef202b902a5c3157bf0662dff91
-
     public Task()
     {
 
     }
     public Task(string Description, string Ailas, bool IsMilestone, DateTime CreatedAtDete, DateTime StartDete, DateTime ScheduledDete, DateTime ForecasDate,
-    DateTime DeadLineDate, DateTime CompleteDate, string Deliverables, string Remarks, int EngineerId, /*לא גמור*/int ComplexityLevl)
+    DateTime DeadLineDate, DateTime CompleteDate, string Deliverables, string Remarks, int EngineerId, /EngineerExperience‏ ComplexityLevl)
     {
         this.Id = Id;
         this.Description = Description;
