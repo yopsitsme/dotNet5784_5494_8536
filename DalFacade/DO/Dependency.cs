4317ab8bde5ae00e2f081d27dd7dffb7e5dafa80
tsw1212 <tsw32595@gmail.com>
2023-11-09 22:48:11 +0200

    stage 1 finish

--- DalFacade/DO/Dependency.cs
+++ DalFacade/DO/Dependency.cs
@@ -1,20 +1,16 @@
 ﻿
 
-
 namespace DO;
 
 // decleration of the entity dependency
 public record Dependency
-{
-    private int idTask1;
-    private int idTask2;
 
     (
-     int Id = 0,
-     int DependentTask = 0,
+     int Id=0,
+     int DependentTask=0,
      int DependsTask = 0
     )
 {
     //defult constructor
-    public Dependency() : this(0, 0, 0) { }
+    public Dependency():this(0,0,0) { }
 }
