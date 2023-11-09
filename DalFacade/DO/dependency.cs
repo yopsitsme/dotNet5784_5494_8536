dd802ea08e242621638342cc2679cc30721e4d01
chana perel kats <chpkats@gmail.com>
2023-11-09 22:43:51 +0200

    try

--- DalFacade/DO/dependency.cs
+++ DalFacade/DO/dependency.cs
@@ -1,19 +1,17 @@
 ﻿
 
+
 namespace DO;
 
 // decleration of the entity dependency
 public record Dependency
-{
-    private int idTask1;
-    private int idTask2;
 
     (
-     int Id=0,
-     int DependentTask=0,
+     int Id = 0,
+     int DependentTask = 0,
      int DependsTask = 0
     )
 {
     //defult constructor
-    public Dependency():this(0,0,0) { }
+    public Dependency() : this(0, 0, 0) { }
 }
