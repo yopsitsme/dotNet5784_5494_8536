1ce7f660faab240ecbf13f54327375dbc462fac8
chana perel kats <chpkats@gmail.com>
2024-02-21 19:41:59 +0200

    try

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -51,7 +51,7 @@ class ConvertCreateSToVisible : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return (bool)value == false ? Visibility.Visible : Visibility.Collapsed;
+        return (bool)value == false ? Visibility.Visible : Visibility.Hidden;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
